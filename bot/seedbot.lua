9a392545c77a9b7562f33b9f1922e89f886748ab
punisherbot <absam14339@yahoo.com>
2016-01-25 06:26:47 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,9 +219,10 @@ function create_config( )
     "download_media",
     "invite",
     "all",
+    "spam",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(168398326)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
